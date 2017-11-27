7b285e61eae9c1e8475ad50669146cefb6374a3e
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-27 19:00:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  const blahBlah = document.getElementsByClassName('target')[0];
+  return blahBlah
+}
+
+function increaseRankBy(n) {
+  var rankList = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < rankList.length; i++) {
+  rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + n
+  }
+  return rankList;
+}
+
+function deepestChild() {
+  let current = document.querySelectorAll(`#grand-node`)[0];
+  let next = current.children[0];
+  while (next) {
+    debugger
+    if (current.length === 1) {
+      return current;
+    } else {
+    current = next;
+    next = current.children[0];
+  }
+}
+return current
+}
